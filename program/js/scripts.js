6b48241daee1b3f29d9424d5535b1f288a53cbe5
amyhhuutt <ahutton2@asu.edu>
2022-04-23 19:35:30 -0700

    PROGRAN BOOK

---
+++ program/js/scripts.js
@@ -0,0 +1,13 @@
+var slider = tns({
+    container: '.my-slider',
+    nav: false,
+    controls: false,
+    items: 3,
+    gutter: 10,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    swipeAngle: false,
+    speed:400,
+    loop: true,
+    autoplayButtonOutput: false
+  });
--- slider/js/scripts.js
+++ slider/js/scripts.js
@@ -1,10 +1,13 @@
-$(document).ready(function(){
-    /*
-    hello hello
-    */
-    $('nav').hide();
-
-    $('.menu-button').click(function(){
-         $('nav').slideToggle();
-    });
-});
+var slider = tns({
+    container: '.my-slider',
+    nav: false,
+    controls: false,
+    items: 3,
+    gutter: 10,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    swipeAngle: false,
+    speed:400,
+    loop: true,
+    autoplayButtonOutput: false
+  });
